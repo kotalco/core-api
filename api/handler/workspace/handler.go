67753875ea2c24ab86d2973f47fe194f0afe7c5d
mohamed-abdelrhman <mohamed95zoytak@gmail.com>
2022-06-03 13:23:23 +0200

    ref: fix typos (closing #31)

--- api/handler/workspace/handler.go
+++ api/handler/workspace/handler.go
@@ -201,7 +201,7 @@ func RemoveMember(c *fiber.Ctx) error {
 	memberId := c.Params("user_id")
 
 	if model.UserId != userId { //check if the user is the owner
-		err := restErrors.NewForbiddenError("you can only delete other users from your own workspac")
+		err := restErrors.NewForbiddenError("you can only delete other users from your own workspace")
 		return c.Status(err.Status).JSON(err)
 	}
 
