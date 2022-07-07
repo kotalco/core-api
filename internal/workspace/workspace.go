94e7ab537e446397c3de3f73e14eb6a505d4b2c2
mohamed-abdelrhman <mohamed95zoytak@gmail.com>
2022-07-07 16:11:31 +0200

    ref:make k8namespace unique field in workspace model

--- internal/workspace/workspace.go
+++ internal/workspace/workspace.go
@@ -5,7 +5,7 @@ import "github.com/kotalco/cloud-api/internal/workspaceuser"
 type Workspace struct {
 	ID             string
 	Name           string
-	K8sNamespace   string                        `gorm:"<-:create"` //allow read and create only
+	K8sNamespace   string                        `gorm:"<-:create;uniqueIndex"` //allow read and create only
 	UserId         string                        `gorm:"index"`
 	WorkspaceUsers []workspaceuser.WorkspaceUser `gorm:"constraint:OnDelete:CASCADE"`
 }
