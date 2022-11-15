513fe6655fe2f2a5510049baf3412b54c26d59ec
mohamed-abdelrhman <mohamed95zoytak@gmail.com>
2022-11-15 11:09:21 +0200

    ref: rebalce managed-by with created-by

--- pkg/k8s/ingressroute/service.go
+++ pkg/k8s/ingressroute/service.go
@@ -63,7 +63,7 @@ func (i *ingressroute) Create(dto *IngressRouteDto) (*traefikv1alpha1.IngressRou
 			Name:            dto.Name,
 			Namespace:       dto.Namespace,
 			OwnerReferences: dto.OwnersRef,
-			Labels:          map[string]string{"app.kubernetes.io/managed-by": "kotal"},
+			Labels:          map[string]string{"app.kubernetes.io/created-by": "kotal"},
 		},
 		Spec: traefikv1alpha1.IngressRouteSpec{
 			EntryPoints: []string{"websecure"},
