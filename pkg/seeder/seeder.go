1208280fd02b44ddae48795002e6e6a9687bad58
mohamed-abdelrhman <mohamed95zoytak@gmail.com>
2022-12-20 11:31:50 +0200

    feat: remove unused seeds code

--- pkg/seeder/seeder.go
+++ pkg/seeder/seeder.go
@@ -4,9 +4,6 @@ import (
 	"errors"
 	"github.com/jackc/pgconn"
 	"github.com/kotalco/cloud-api/internal/setting"
-	"github.com/kotalco/cloud-api/internal/user"
-	"github.com/kotalco/cloud-api/internal/verification"
-	"github.com/kotalco/cloud-api/internal/workspace"
 	"gorm.io/gorm"
 )
 
@@ -19,9 +16,6 @@ type seeder struct {
 }
 
 type ISeeder interface {
-	SeedUserTable(users *user.User) error
-	SeedVerificationTable(verifications *verification.Verification) error
-	SeedWorkspaceTable(workspace *workspace.Workspace) error
 	SeedSettingTable(setting *setting.Setting) error
 }
 
@@ -29,40 +23,6 @@ func NewSeeder(dbClient *gorm.DB) ISeeder {
 	newSeeder := new(seeder)
 	newSeeder.dbClient = dbClient
 	return newSeeder
-}
-
-func (s seeder) SeedUserTable(user *user.User) error {
-	res := s.dbClient.FirstOrCreate(user)
-	if res.Error != nil {
-		var pgErr *pgconn.PgError
-		if errors.As(res.Error, &pgErr) {
-			if pgErr.Code != "23505" {
-				return res.Error
-			}
-		}
-	}
-	return nil
-}
-
-func (s seeder) SeedVerificationTable(verification *verification.Verification) error {
-	res := s.dbClient.FirstOrCreate(verification)
-	if res.Error != nil {
-		return res.Error
-	}
-	return nil
-}
-
-func (s seeder) SeedWorkspaceTable(workspace *workspace.Workspace) error {
-	res := s.dbClient.FirstOrCreate(workspace)
-	if res.Error != nil {
-		var pgErr *pgconn.PgError
-		if errors.As(res.Error, &pgErr) {
-			if pgErr.Code != "23505" {
-				return res.Error
-			}
-		}
-	}
-	return nil
 }
 
 func (s seeder) SeedSettingTable(setting *setting.Setting) error {
--- pkg/seeder/service.go
+++ pkg/seeder/service.go
@@ -1,12 +1,7 @@
 package seeder
 
 import (
-	"github.com/google/uuid"
 	"github.com/kotalco/cloud-api/internal/setting"
-	"github.com/kotalco/cloud-api/internal/user"
-	"github.com/kotalco/cloud-api/internal/workspace"
-	"github.com/kotalco/cloud-api/internal/workspaceuser"
-	"github.com/kotalco/cloud-api/pkg/roles"
 	"github.com/kotalco/cloud-api/pkg/security"
 	"github.com/kotalco/community-api/pkg/logger"
 	"gorm.io/gorm"
@@ -38,42 +33,7 @@ func NewService(dbClient *gorm.DB) IService {
 }
 
 func (s service) Seeds() map[string]Definition {
-	defaultUserId := uuid.NewString()
 	return map[string]Definition{
-		SeedUsersTable: {
-			Run: func() error {
-				hashedPassword, err := hashing.Hash("develop", 13)
-				if err != nil {
-					go logger.Error("SEEDING_HASHING_ERR", err)
-				}
-				developUser := new(user.User)
-				developUser.ID = defaultUserId
-				developUser.Email = "develop@kotal.co"
-				developUser.IsEmailVerified = true
-				developUser.Password = string(hashedPassword)
-				developUser.PlatformAdmin = true
-				return seeders.SeedUserTable(developUser)
-			},
-		},
-		SeedWorkspaceTable: {
-			Run: func() error {
-				newWorkspace := new(workspace.Workspace)
-				newWorkspace.ID = uuid.New().String()
-				newWorkspace.UserId = defaultUserId
-				newWorkspace.Name = "default"
-				newWorkspace.K8sNamespace = "default"
-				//create workspace-user record
-				newWorkspaceuser := new(workspaceuser.WorkspaceUser)
-				newWorkspaceuser.ID = uuid.New().String()
-				newWorkspaceuser.WorkspaceID = newWorkspace.ID
-				newWorkspaceuser.UserId = newWorkspace.UserId
-				newWorkspaceuser.Role = roles.Admin
-
-				newWorkspace.WorkspaceUsers = append(newWorkspace.WorkspaceUsers, *newWorkspaceuser)
-
-				return seeders.SeedWorkspaceTable(newWorkspace)
-			},
-		},
 		SeedSettingTable: {
 			Run: func() error {
 				record := new(setting.Setting)
