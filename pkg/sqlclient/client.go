37b7b9d586d8faedfcf3d19cc514029bcaff6cd2
mohamed-abdelrhman <mohamed95zoytak@gmail.com>
2022-12-20 21:54:35 +0200

    feat: remove unuesed code

--- pkg/sqlclient/client.go
+++ pkg/sqlclient/client.go
@@ -47,22 +47,3 @@ func Rollback(txHandle *gorm.DB) {
 func Commit(txHandle *gorm.DB) {
 	txHandle.Commit()
 }
-
-func Transact(db *gorm.DB, txFunc func(tx *gorm.DB) error) (err error) {
-	tx := db.Begin()
-	if err != nil {
-		return
-	}
-	defer func() {
-		if p := recover(); p != nil {
-			tx.Rollback()
-			panic(p) // re-throw panic after Rollback
-		} else if err != nil {
-			tx.Rollback() // err is non-nil; don't change it
-		} else {
-			tx.Commit() // err is nil; if Commit returns error update err
-		}
-	}()
-	err = txFunc(tx)
-	return err
-}
